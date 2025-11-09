ea8fe44f7534d3fda987b373d776b13f37a71e27
Manikandan-1028 <manikandandq22@gmail.com>
2025-11-09 14:16:32 +0530

    Fix signup controller: single response and token handling

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,52 +5,113 @@ const User = require('../models/userModel')
 const bcrypt = require('bcrypt')
 const generateToken  = require('../utils/generateToken')
 
+// const signUp = async (req, res) => {
+//     try {
+//         const {username, email, password } = req.body;
+//         //email validation
+//         const emailValidation = /^[^\$@]+@[^\$@]+\.[^\$@]+$/;
+//         if(!emailValidation.test(email)){
+//             res.status(400).json({"error":"invalid email"})
+//         }
+
+//         //validation
+//         const userExists = await User.findOne({username})
+//         const emailExists = await User.findOne({email})
+
+//         if (userExists || emailExists) {
+//            return res.status(400).json("username or email already exists")
+//         }
+
+//         if(password.length < 6){
+//             res.status(400).json("password should have 6 characters")
+//         }
+//         //hashing password
+//         const salt = await bcrypt.genSalt(10)
+//         const hashedPassword = await bcrypt.hash(password,salt)
+
+//         const newUser = new User({
+//             username,
+//             email,
+//             password:hashedPassword
+//         })
+
+//         //saving data in db
+//         if(newUser){
+//             //generating token
+//             generateToken(newUser._id,res)
+//             await newUser.save()
+//             res.status(200).json("user created successfully.")
+//         }
+//         else{
+//             res.status(400).json("user not created")
+//         }
+//     }
+//     catch(err){
+//         console.log("error in signup controller",err)
+//         res.status(400).json("internal server error",err)
+//     }
+// }
 const signUp = async (req, res) => {
-    try {
-        const {username, email, password } = req.body;
-        //email validation
-        const emailValidation = /^[^\$@]+@[^\$@]+\.[^\$@]+$/;
-        if(!emailValidation.test(email)){
-            res.status(400).json({"error":"invalid email"})
-        }
+  try {
+    const { username, email, password } = req.body;
 
-        //validation
-        const userExists = await User.findOne({username})
-        const emailExists = await User.findOne({email})
-
-        if (userExists || emailExists) {
-           return res.status(400).json("username or email already exists")
-        }
-
-        if(password.length < 6){
-            res.status(400).json("password should have 6 characters")
-        }
-        //hashing password
-        const salt = await bcrypt.genSalt(10)
-        const hashedPassword = await bcrypt.hash(password,salt)
-
-        const newUser = new User({
-            username,
-            email,
-            password:hashedPassword
-        })
-
-        //saving data in db
-        if(newUser){
-            //generating token
-            generateToken(newUser._id,res)
-            await newUser.save()
-            res.status(200).json("user created successfully.")
-        }
-        else{
-            res.status(400).json("user not created")
-        }
+    // Email validation
+    const emailValidation = /^[^\$@]+@[^\$@]+\.[^\$@]+$/;
+    if (!emailValidation.test(email)) {
+      return res.status(400).json({ error: "invalid email" }); // return here!
     }
-    catch(err){
-        console.log("error in signup controller",err)
-        res.status(400).json("internal server error",err)
+
+    // Check if user/email exists
+    const userExists = await User.findOne({ username });
+    const emailExists = await User.findOne({ email });
+
+    if (userExists || emailExists) {
+      return res.status(400).json("username or email already exists");
     }
-}
+
+    // Password length validation
+    if (password.length < 6) {
+      return res.status(400).json("password should have at least 6 characters");
+    }
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create new user
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    // Save user to DB first
+    await newUser.save();
+
+    // Generate token
+    const token = generateToken(newUser._id); // Modified generateToken to return token
+
+    // Send response with cookie and token
+    res.cookie("jwt", token, {
+      maxAge: 14 * 24 * 60 * 60 * 1000,
+      httpOnly: true,
+      sameSite: "strict",
+    });
+
+    res.status(200).json({
+      message: "User created successfully",
+      user: {
+        _id: newUser._id,
+        username: newUser.username,
+        email: newUser.email,
+      },
+      token,
+    });
+  } catch (err) {
+    console.log("error in signup controller", err);
+    res.status(500).json({ error: "internal server error" });
+  }
+};
 
 
 const login = async (req, res) => {
